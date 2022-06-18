0401338d057c170bee2dfc357ff6287a0a9bc431
ujala-singh <ujala.singh@razorpay.com>
2022-06-18 23:23:08 +0530

    Update

--- common/net/Jira.js
+++ common/net/Jira.js
@@ -21,62 +21,6 @@ class Jira {
       method: 'GET',
     })
   }
-
-  async fetch (apiMethodName,
-    { host, pathname, query },
-    { method, body, headers = {} } = {}) {
-    const url = format({
-      host: host || this.baseUrl,
-      pathname,
-      query,
-    })
-
-    if (!method) {
-      method = 'GET'
-    }
-
-    if (headers['Content-Type'] === undefined) {
-      headers['Content-Type'] = 'application/json'
-    }
-
-    if (headers.Authorization === undefined) {
-      headers.Authorization = `Basic ${Buffer.from(`${this.email}:${this.token}`).toString('base64')}`
-    }
-
-    // strong check for undefined
-    // cause body variable can be 'false' boolean value
-    if (body && headers['Content-Type'] === 'application/json') {
-      body = JSON.stringify(body)
-    }
-
-    const state = {
-      req: {
-        method,
-        headers,
-        body,
-        url,
-      },
-    }
-
-    try {
-      await client(state, `${serviceName}:${apiMethodName}`)
-    } catch (error) {
-      const fields = {
-        originError: error,
-        source: 'jira',
-      }
-
-      delete state.req.headers
-
-      throw Object.assign(
-        new Error('Jira API error'),
-        state,
-        fields
-      )
-    }
-
-    return state.res.body
-  }
 }
 
 module.exports = Jira
